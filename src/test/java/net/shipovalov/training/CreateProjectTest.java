e0eaadec5f199c63d7573ccc21429f7ba6393a5a
st15501 <st15501@ptc.local>
2018-06-14 13:10:42 +0300

    add test for mantis project issue

--- src/test/java/net/shipovalov/training/CreateProjectTest.java
+++ src/test/java/net/shipovalov/training/CreateProjectTest.java
@@ -1,14 +1,13 @@
 package net.shipovalov.training;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.*;
 
 public class CreateProjectTest {
     WebDriver driver;
--- src/test/java/net/shipovalov/training/CreateUssueTest.java
+++ src/test/java/net/shipovalov/training/CreateUssueTest.java
@@ -1,25 +1,20 @@
 package net.shipovalov.training;
 
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.*;
-import static org.openqa.selenium.OutputType.*;
 
 public class CreateUssueTest {
     WebDriver wd;
     
     @BeforeMethod
     public void setUp() throws Exception {
-        wd = new FirefoxDriver();
+        wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     }
     
