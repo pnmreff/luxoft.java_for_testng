9a0883ea23470cdb605bdeddf0f4826d24440c17
st15501 <st15501@ptc.local>
2018-06-14 15:58:34 +0300

    add notes, method for search JS elements

--- src/test/java/net/shipovalov/training/CreateProjectTest.java
+++ src/test/java/net/shipovalov/training/CreateProjectTest.java
@@ -1,14 +1,13 @@
 package net.shipovalov.training;
 
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
 
 public class CreateProjectTest {
@@ -18,15 +17,14 @@ public class CreateProjectTest {
     public void setUp() throws Exception {
         System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver.exe");
 //        System.setProperty("webdriver.ie.driver", "src\\test\\resources\\IEDriverServer.exe");
-        driver = new InternetExplorerDriver();
+        driver = new new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-
+        openLoginPage();
+        login();
     }
-    
-    @Test
-    public void createProjectTest() {
-        driver.get("http://shipovalov.net/login_page.php");
+
+    private void login() {
         driver.findElement(By.name("username")).click();
         driver.findElement(By.name("username")).clear();
         driver.findElement(By.name("username")).sendKeys("student");
@@ -34,22 +32,62 @@ public class CreateProjectTest {
         driver.findElement(By.name("password")).clear();
         driver.findElement(By.name("password")).sendKeys("luxoft");
         driver.findElement(By.cssSelector("input.button")).click();
-        driver.findElement(By.linkText("Manage")).click();
-        driver.findElement(By.linkText("Manage Projects")).click();
-        driver.findElement(By.xpath("//table[3]/tbody/tr[1]/td/form/input[2]")).click();
+        selectProject();
+    }
+
+    private void openLoginPage() {
+        driver.get("http://shipovalov.net/login_page.php");
+    }
+
+    @Test
+    public void createProjectTest() {
+        openManagePage();
+        openManageProjectPage();
+        initProjectCreation();
+        fillProjectForm();
+        submitProjectForm();
+
+    }
+
+    private void submitProjectForm() {
+        driver.findElement(By.cssSelector("input.button")).click();
+    }
+
+    private void fillProjectForm() {
         driver.findElement(By.name("name")).click();
         driver.findElement(By.name("name")).clear();
         driver.findElement(By.name("name")).sendKeys("Championchip gor2014");
         driver.findElement(By.name("description")).click();
         driver.findElement(By.name("description")).clear();
         driver.findElement(By.name("description")).sendKeys("Yhis is tets fggfghghfgh");
-        driver.findElement(By.cssSelector("input.button")).click();
-        driver.findElement(By.linkText("Logout")).click();
     }
-    
+
+    private void initProjectCreation() {
+        driver.findElement(By.xpath("//table[3]/tbody/tr[1]/td/form/input[2]")).click();
+    }
+
+    private void openManageProjectPage() {
+        driver.findElement(By.linkText("Manage Projects")).click();
+    }
+
+    private void openManagePage() {
+        driver.findElement(By.linkText("Manage")).click();
+    }
+
     @AfterMethod
     public void tearDown() {
+        logout();
         driver.quit();
     }
 
+    private void logout() {
+        driver.findElement(By.linkText("Logout")).click();
+    }
+
+
+
+    private void selectProject(){
+        Select select = new Select(driver.findElement(By.name("project_id")));
+        select.selectByVisibleText("11122");
+    }
 }
--- src/test/java/net/shipovalov/training/CreateUssueTest.java
+++ src/test/java/net/shipovalov/training/CreateUssueTest.java
@@ -22,18 +22,17 @@ public class CreateUssueTest {
     
     @Test
     public void CreateUssuesTest() {
-        webDriver.get("http://shipovalov.net/login_page.php");
-        webDriver.findElement(By.cssSelector("div")).click();
-        webDriver.findElement(By.name("username")).click();
-        webDriver.findElement(By.name("username")).clear();
-        webDriver.findElement(By.name("username")).sendKeys("student");
-        webDriver.findElement(By.name("password")).click();
-        webDriver.findElement(By.name("password")).clear();
-        webDriver.findElement(By.name("password")).sendKeys("luxoft");
-        webDriver.findElement(By.cssSelector("input.button")).click();
-        if (!webDriver.findElement(By.xpath("//td[@class='login-info-right']/form/select//option[49]")).isSelected()) {
-            webDriver.findElement(By.xpath("//td[@class='login-info-right']/form/select//option[49]")).click();
-        }
+        fillLoginForm();
+        selectProject();
+        CreateIssue();
+        Logout();
+    }
+
+    private void Logout() {
+        webDriver.findElement(By.linkText("Logout")).click();
+    }
+
+    private void CreateIssue() {
         webDriver.findElement(By.linkText("Report Issue")).click();
         if (!webDriver.findElement(By.xpath("//div[3]/form/table/tbody/tr[2]/td[2]/select//option[2]")).isSelected()) {
             webDriver.findElement(By.xpath("//div[3]/form/table/tbody/tr[2]/td[2]/select//option[2]")).click();
@@ -73,11 +72,26 @@ public class CreateUssueTest {
         webDriver.findElement(By.name("additional_info")).clear();
         webDriver.findElement(By.name("additional_info")).sendKeys("none");
         webDriver.findElement(By.cssSelector("input.button")).click();
-        webDriver.findElement(By.linkText("Logout")).click();
-        webDriver.findElement(By.name("username")).click();
-        webDriver.findElement(By.name("username")).sendKeys("\\9");
     }
-    
+
+    private void selectProject() {
+        if (!webDriver.findElement(By.xpath("//td[@class='login-info-right']/form/select//option[49]")).isSelected()) {
+            webDriver.findElement(By.xpath("//td[@class='login-info-right']/form/select//option[49]")).click();
+        }
+    }
+
+    private void fillLoginForm() {
+        webDriver.get("http://shipovalov.net/login_page.php");
+        webDriver.findElement(By.cssSelector("div")).click();
+        webDriver.findElement(By.name("username")).click();
+        webDriver.findElement(By.name("username")).clear();
+        webDriver.findElement(By.name("username")).sendKeys("student");
+        webDriver.findElement(By.name("password")).click();
+        webDriver.findElement(By.name("password")).clear();
+        webDriver.findElement(By.name("password")).sendKeys("luxoft");
+        webDriver.findElement(By.cssSelector("input.button")).click();
+    }
+
     @AfterMethod
     public void tearDown() {
         webDriver.quit();
